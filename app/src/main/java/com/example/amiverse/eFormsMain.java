7c9761d466375b8f79f6035dcadcb03a4c1daee0
SonalPathak108 <64786900+SonalPathak108@users.noreply.github.com>
2020-06-02 21:26:08 +0530

    02-06-20 basic changes in the UI
    ● The background colour of splash sceen changed
    
    ● The logo made uniform
    
    ● The sentence case maintained in text
    
    ● size of Logo and the page header changed
    
    ● Colour combination changed
    
    ●some changes on specific pages like
    
    background of Event registeration,
    
    buttons,
    
    clipart change on home page
    
    indentation of AUMP Clubs

--- app/src/main/java/com/example/amiverse/eFormsMain.java
+++ app/src/main/java/com/example/amiverse/eFormsMain.java
@@ -1,14 +1,25 @@
 package com.example.amiverse;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+
 import androidx.appcompat.widget.Toolbar;
 
 public class eFormsMain extends AppCompatActivity {
-
+    Button oval;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eforms_main);
+        oval = (Button) findViewById(R.id.oval);
+        oval.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getApplicationContext(),"Register!",Toast.LENGTH_SHORT).show();
+        }
+    });
     }
 
 }
