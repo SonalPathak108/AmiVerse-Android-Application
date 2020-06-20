942f153175a5f6b7e24d94ff86e745d20f2b2e68
surbhi820 <subhig820@gmail.com>
2020-06-20 15:26:54 +0530

    admin scoreboard files

---
+++ app/src/main/java/com/example/amiverse/Adminscoreboard.java
@@ -0,0 +1,35 @@
+package com.example.amiverse;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Adminscoreboard extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_scoreboard);
+       Spinner spinner =findViewById(R.id.spinner1);
+       ArrayAdapter<CharSequence> adapter= ArrayAdapter.createFromResource(this,R.array.Block,android.R.layout.simple_spinner_item);
+       adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+       spinner.setAdapter(adapter);
+       spinner.setOnItemSelectedListener(this);
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
--- app/src/main/java/com/example/amiverse/Score_board.java
+++ app/src/main/java/com/example/amiverse/Score_board.java
@@ -1,27 +1,53 @@
 package com.example.amiverse;
 
-import android.app.Activity;
+//import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+//import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Score_board extends AppCompatActivity {
 
+//TextView textview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score_board);
+//        TextView  textview =(TextView) findViewById(R.id.tv1);
+//        TextView  textview1 =(TextView) findViewById(R.id.tv2);
+//        TextView  textview2 =(TextView) findViewById(R.id.tv3);
+//        TextView  textview3 =(TextView) findViewById(R.id.tv4);
+//        TextView  textview4 =(TextView) findViewById(R.id.tv5);
+//        TextView  textview5 =(TextView) findViewById(R.id.tv6);
+//        TextView  textview6 =(TextView) findViewById(R.id.tv7);
+//        TextView  textview7 =(TextView) findViewById(R.id.tv8);
+//        TextView  textview8 =(TextView) findViewById(R.id.tv9);
+//        TextView  textview9 =(TextView) findViewById(R.id.tv10);
+
+//        TextView  textview11 =(TextView) findViewById(R.id.tv11);
+//        TextView  textview12 =(TextView) findViewById(R.id.tv12);
+//        TextView  textview13 =(TextView) findViewById(R.id.tv13);
+//        TextView  textview14 =(TextView) findViewById(R.id.tv14);
+//        TextView  textview15=(TextView) findViewById(R.id.tv15);
+//        TextView  textview16 =(TextView) findViewById(R.id.tv16);
+//        TextView  textview17=(TextView) findViewById(R.id.tv17);
+//        TextView  textview18=(TextView) findViewById(R.id.tv18);
+//        TextView  textview19 =(TextView) findViewById(R.id.tv19);
+//        TextView  textview20=(TextView) findViewById(R.id.tv20);
+//        TextView  textview21=(TextView) findViewById(R.id.tv21);
+//        TextView  textview22 =(TextView) findViewById(R.id.tv22);
+//        TextView  textview23=(TextView) findViewById(R.id.tv23);
+//        TextView  textview24=(TextView) findViewById(R.id.tv24);
+//        TextView  textview25=(TextView) findViewById(R.id.tv25);
+//        TextView  textview26=(TextView) findViewById(R.id.tv26);
+//        TextView  textview27 =(TextView) findViewById(R.id.tv27);
+//        TextView  textview28=(TextView) findViewById(R.id.tv28);
+
 
     }
-
-
-
-
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
@@ -38,9 +64,10 @@ public class Score_board extends AppCompatActivity {
             case R.id.notice:
                 return true;
             case R.id.announcement:
-                return true;
+               return true;
             case R.id.resultpdf:
-                return true;
+                return  true;
+
 
         }
         return super.onOptionsItemSelected(item);
