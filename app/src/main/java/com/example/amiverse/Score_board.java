e79b739e1773f98064e58aa2f7b6548cc857315e
surbhi820 <subhig820@gmail.com>
2020-06-21 15:46:32 +0530

    scoreboard correction

--- app/src/main/java/com/example/amiverse/Score_board.java
+++ app/src/main/java/com/example/amiverse/Score_board.java
@@ -16,35 +16,35 @@ public class Score_board extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score_board);
-//        TextView  textview =(TextView) findViewById(R.id.tv1);
-//        TextView  textview1 =(TextView) findViewById(R.id.tv2);
-//        TextView  textview2 =(TextView) findViewById(R.id.tv3);
-//        TextView  textview3 =(TextView) findViewById(R.id.tv4);
-//        TextView  textview4 =(TextView) findViewById(R.id.tv5);
-//        TextView  textview5 =(TextView) findViewById(R.id.tv6);
-//        TextView  textview6 =(TextView) findViewById(R.id.tv7);
-//        TextView  textview7 =(TextView) findViewById(R.id.tv8);
-//        TextView  textview8 =(TextView) findViewById(R.id.tv9);
-//        TextView  textview9 =(TextView) findViewById(R.id.tv10);
+//        TextView  textview =(TextView) findViewById(R.id.Abs_gold);
+//        TextView  textview1 =(TextView) findViewById(R.id.Abs_silver);
+//        TextView  textview2 =(TextView) findViewById(R.id.Abs_bronze);
+//        TextView  textview3 =(TextView) findViewById(R.id.Abs_score);
+//        TextView  textview4 =(TextView) findViewById(R.id.Als_gold);
+//        TextView  textview5 =(TextView) findViewById(R.id.Als_silver);
+//        TextView  textview6 =(TextView) findViewById(R.id.Als_bronze);
+//        TextView  textview7 =(TextView) findViewById(R.id.Als_score);
+//        TextView  textview8 =(TextView) findViewById(R.id.Aset_gold);
+//        TextView  textview9 =(TextView) findViewById(R.id.Aset_silver);
 
-//        TextView  textview11 =(TextView) findViewById(R.id.tv11);
-//        TextView  textview12 =(TextView) findViewById(R.id.tv12);
-//        TextView  textview13 =(TextView) findViewById(R.id.tv13);
-//        TextView  textview14 =(TextView) findViewById(R.id.tv14);
-//        TextView  textview15=(TextView) findViewById(R.id.tv15);
-//        TextView  textview16 =(TextView) findViewById(R.id.tv16);
-//        TextView  textview17=(TextView) findViewById(R.id.tv17);
-//        TextView  textview18=(TextView) findViewById(R.id.tv18);
-//        TextView  textview19 =(TextView) findViewById(R.id.tv19);
-//        TextView  textview20=(TextView) findViewById(R.id.tv20);
-//        TextView  textview21=(TextView) findViewById(R.id.tv21);
-//        TextView  textview22 =(TextView) findViewById(R.id.tv22);
-//        TextView  textview23=(TextView) findViewById(R.id.tv23);
-//        TextView  textview24=(TextView) findViewById(R.id.tv24);
-//        TextView  textview25=(TextView) findViewById(R.id.tv25);
-//        TextView  textview26=(TextView) findViewById(R.id.tv26);
-//        TextView  textview27 =(TextView) findViewById(R.id.tv27);
-//        TextView  textview28=(TextView) findViewById(R.id.tv28);
+//        TextView  textview11 =(TextView) findViewById(R.id.Aset_bronze);
+//        TextView  textview12 =(TextView) findViewById(R.id.Aset_score);
+//        TextView  textview13 =(TextView) findViewById(R.id.Asco_gold);
+//        TextView  textview14 =(TextView) findViewById(R.id.Asco_silver);
+//        TextView  textview15=(TextView) findViewById(R.id.Asco_bronze);
+//        TextView  textview16 =(TextView) findViewById(R.id.Asco_score;
+//        TextView  textview17=(TextView) findViewById(R.id.Aibas_gold);
+//        TextView  textview18=(TextView) findViewById(R.id.Aibas_silver);
+//        TextView  textview19 =(TextView) findViewById(R.id.Aibas_bronze);
+//        TextView  textview20=(TextView) findViewById(R.id.Aibas_score);
+//        TextView  textview21=(TextView) findViewById(R.id.Asp_gold);
+//        TextView  textview22 =(TextView) findViewById(R.id.Asp_silver);
+//        TextView  textview23=(TextView) findViewById(R.id.Asp_bronze);
+//        TextView  textview24=(TextView) findViewById(R.id.Asp_score);
+//        TextView  textview25=(TextView) findViewById(R.id.Afd_gold);
+//        TextView  textview26=(TextView) findViewById(R.id.Afd_silver);
+//        TextView  textview27 =(TextView) findViewById(R.id.Afd_bronze);
+//        TextView  textview28=(TextView) findViewById(R.id.Afd_score);
 
 
     }
