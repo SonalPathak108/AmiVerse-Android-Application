831b730ffe040802663e350fabd42a034e0eab57
mukul56 <amukul56@gmail.com>
2020-07-03 22:33:42 +0530

    Hostel layouts implemented

--- app/src/main/java/com/example/amiverse/RegistrationActivity.java
+++ app/src/main/java/com/example/amiverse/RegistrationActivity.java
@@ -13,7 +13,6 @@ import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
-import com.android.volley.Response.ErrorListener;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
